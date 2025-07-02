f272484358077462037f1fa15a7c1c7192922374
luanahonorio.ferreira@gmail.com <luanahonorio.ferreira@gmail.com>
2025-07-02 14:20:39 +0100

    usa variáveis de ambiente para o Firebase

---
+++ firebaseauth-with-auth-context-luana/firebaseConfig.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const database = firebase.firestore();
+export const auth = firebase.auth();
